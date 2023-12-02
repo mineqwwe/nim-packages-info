fdd58ea4d89b6112936aa59e7d2071a27f52d4da
mineqwwe <69395985+mineqwwe@users.noreply.github.com>
2023-12-02 11:28:26 +0800

    Add files via upload

---
+++ info.py
@@ -0,0 +1,79 @@
+import json
+import requests
+import datetime
+
+
+def get_github_repo_info(repo_url):
+    # 提取仓库名称和作者
+    repo_url = repo_url.rstrip("/")
+    parts = repo_url.split('/')
+    username = parts[-2]
+    repo_name = parts[-1].split('.')[0]
+
+    # 构建API请求URL
+    api_url = f"https://api.github.com/repos/{username}/{repo_name}"
+
+    # 发起GET请求
+    print("api_url: ",api_url)
+    response = requests.get(api_url)
+    print(response.status_code)
+    if response.status_code == 200:
+        data = response.json()
+        # 提取更新时间和star数
+        last_updated = data['updated_at']
+        stars = data['stargazers_count']
+        # 转换为datetime对象并返回
+        update_time = datetime.datetime.strptime(
+            last_updated, "%Y-%m-%dT%H:%M:%SZ")
+        return update_time, stars
+    else:
+        # 请求失败时返回空值
+        return None, None
+
+
+def get_repo_info(file_path):
+    repo_array = []
+    with open(file_path, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+        for repo in data:
+            url = repo.get('url')
+            web = repo.get('web')
+            name = repo.get('name')
+            description = repo.get('description')
+            if url is None:
+                urls = web
+            else:
+                urls = url
+            repo_dict = {
+                "name": name,
+                "urls": urls,
+                "description" : description
+            }
+            repo_array.append(repo_dict)
+    return repo_array
+
+
+repo_array = get_repo_info("packages_official.json")
+
+
+import csv
+
+# 假设repo_array是你的数据列表
+
+# 打开一个CSV文件进行写入
+with open('repo_info.csv', 'w', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+
+    # 写入表头
+    writer.writerow(["name", "stars", "update_time", "description", "urls"])
+
+    # 遍历repo_array，并写入每个repo的信息
+    for repo in repo_array:
+        name = repo.get("name")
+        # print(name)
+        urls = repo.get("urls")
+        description = repo.get("description")
+        update_time, stars = get_github_repo_info(urls)
+
+        # 将每个repo的信息写入CSV文件
+        writer.writerow([name, stars, update_time, description, urls])
